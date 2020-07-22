cb5328297f0acd9d3bf5bdd38046b8e4067dde09
Shubham Kumar <shubhamkumar65553@gmail.com>
2020-07-22 16:11:36 +0530

    technews app

---
+++ src/App.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import App, { Search, Button, Table } from './App';
+
+describe('Search', () => {
+  it('renders', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Search>Search</Search>, div);
+  });
+  test('snapshots', () => {
+  const component = renderer.create(
+  <Search>Search</Search>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+  });
+  });
+
+ describe('Button', () => {
+it('renders', () => {
+const div = document.createElement('div');
+ReactDOM.render(<Button>Give Me More</Button>, div);
+});
+test('snapshots', () => {
+const component = renderer.create(
+<Button>Give Me More</Button>
+);
+let tree = component.toJSON();
+expect(tree).toMatchSnapshot();
+});
+});
+
+describe('Table', () => {
+  const props = {
+  list: [
+  { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+  { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+  ],
+  sortKey: 'TITLE',
+  isSortReverse: false,
+  };
+  it('renders', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Table { ...props } />, div);
+  });
+  test('snapshots', () => {
+  const component = renderer.create(
+  <Table { ...props } />
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+});
